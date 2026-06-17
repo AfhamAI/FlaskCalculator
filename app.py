b9d3db52216ddddfd6b5822f99df75e9d80be590
afham <afhamtab@gmail.com>
2026-06-17 11:33:14 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    expression = ""
+    if request.method == 'POST':
+        expression = request.form.get('expression')
+        value = request.form.get('value')
+        if value == "C":
+            expression = ""
+        elif value == "=":
+            try : 
+                expression = str(eval(expression))
+            except:
+                expression = "Invalid Input"
+        else:
+            expression += value
+    return render_template('index.html' , expression = expression)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+'''
+    expression = ""
+
+    if request.method == "POST":
+        expression = request.form.get("expression")
+        value = request.form.get("value")
+
+        if value == "C":
+            expression = ""
+        elif value == "=":
+            try:
+                expression = str(eval(expression))
+            except:
+                expression = "Error"
+        else:
+            expression += value
+
+    return render_template("index.html", expression=expression)
+'''
