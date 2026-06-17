0b17f305d87fd4406092f6bb8f7e02411b0e8600
Afham <afhamtab@gmail.com>
2026-06-17 11:37:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,27 +22,3 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-'''
-    expression = ""
-
-    if request.method == "POST":
-        expression = request.form.get("expression")
-        value = request.form.get("value")
-
-        if value == "C":
-            expression = ""
-        elif value == "=":
-            try:
-                expression = str(eval(expression))
-            except:
-                expression = "Error"
-        else:
-            expression += value
-
-    return render_template("index.html", expression=expression)
-'''
